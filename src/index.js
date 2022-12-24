fa16dbef7f2b379910121bd298aa88d9e1eb0af2
javiriya bathool <106423604+Byte786@users.noreply.github.com>
2022-12-24 19:39:10 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-
-
-
+<div>
+<button onclick={this.handel.click}>
+  click here when onclick A is ran
+  </button>
+</div>
 ReactDOM.render(<App />, document.getElementById("root"));
